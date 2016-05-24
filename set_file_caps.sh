dfeb21931f256d0bb8232a61eaa302164937ad04
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2016-05-24 18:23:23 +0200

    add some first measurements

--- set_file_caps.sh
+++ set_file_caps.sh
@@ -7,21 +7,27 @@ echo "+----------------------------------------+"
 echo " "
 
 if [ -e example1 ]; then
-    sudo setcap "cap_sys_nice=p" ./example1
+    sudo setcap "cap_sys_nice=pe" ./example1
 else
     echo "example1 does not exist"
 fi
 
 if [ -e example_gpio ]; then
-    sudo setcap "cap_sys_nice=p" ./example_gpio
+    sudo setcap "cap_sys_nice=pe" ./example_gpio
 else
     echo "example_gpio does not exist"
 fi
 
 if [ -e example_gpio2 ]; then
-    sudo setcap "cap_sys_nice=p" ./example_gpio2
+    sudo setcap "cap_sys_nice=pe" ./example_gpio2
 else
     echo "example_gpio2 does not exist"
+fi
+
+if [ -e example_swt_led ]; then
+    sudo setcap "cap_sys_nice=pe" ./example_swt_led
+else
+    echo "example_swt_led does not exist"
 fi
 
 echo " "

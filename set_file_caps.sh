90005b79317ff7eca040d25a444063fc3b393df7
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2017-06-04 21:18:38 +0200

    fix set file caps script

--- set_file_caps.sh
+++ set_file_caps.sh
@@ -2,57 +2,63 @@
 
 echo " "
 echo "+----------------------------------------+"
-echo "|   Try to set file capabilities         |"
+echo "| Try to set file capabilities           |"
+echo "| --> need sudo for some parts           |"
 echo "+----------------------------------------+"
 echo " "
 
+sudo -v
+# keep-alive
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
 # ttenv_gpio
-if [ -e ./ttenv_gpio/ttenv_gpio ]; then
+if [ -f ./ttenv_gpio/ttenv_gpio ]; then
     sudo setcap "cap_sys_nice=pe" ./ttenv_gpio/ttenv_gpio
 else
     echo "./ttenv_gpio/ttenv_gpio does not exist"
 fi
 
-if [ -e ${HOME}/bin/ttenv_gpio ]; then
+if [ -f ${HOME}/bin/ttenv_gpio ]; then
     sudo setcap "cap_sys_nice=pe" ${HOME}/bin/ttenv_gpio
 else
     echo "${HOME}/bin/ttenv_gpio does not exist"
 fi
 
 # ttenv_gpio_simple
-if [ -e ./ttenv_gpio_simple/ttenv_gpio_simple ]; then
+if [ -f ./ttenv_gpio_simple/ttenv_gpio_simple ]; then
     sudo setcap "cap_sys_nice=pe" ./ttenv_gpio_simple/ttenv_gpio_simple
 else
     echo "./ttenv_gpio_simple/ttenv_gpio_simple does not exist"
 fi
 
-if [ -e ${HOME}/bin/ttenv_gpio_simple ]; then
+if [ -f ${HOME}/bin/ttenv_gpio_simple ]; then
     sudo setcap "cap_sys_nice=pe" ${HOME}/bin/ttenv_gpio_simple
 else
     echo "${HOME}/bin/ttenv_gpio_simple does not exist"
 fi
 
 # ttenv_simple
-if [ -e ./ttenv_simple/ttenv_simple ]; then
+if [ -f ./ttenv_simple/ttenv_simple ]; then
     sudo setcap "cap_sys_nice=pe" ./ttenv_simple/ttenv_simple
 else
     echo "./ttenv_simple/ttenv_simple does not exist"
 fi
 
-if [ -e ${HOME}/bin/ttenv_simple ]; then
+if [ -f ${HOME}/bin/ttenv_simple ]; then
     sudo setcap "cap_sys_nice=pe" ${HOME}/bin/ttenv_simple
 else
     echo "${HOME}/bin/ttenv_simple does not exist"
 fi
 
 # ttenv_swt_led
-if [ -e ./ttenv_swt_led/ttenv_swt_led ]; then
+if [ -f ./ttenv_swt_led/ttenv_swt_led ]; then
     sudo setcap "cap_sys_nice=pe" ./ttenv_swt_led/ttenv_swt_led
 else
     echo "./ttenv_swt_led/ttenv_swt_led does not exist"
 fi
 
-if [ -e ${HOME}/bin/ttenv_swt_led ]; then
+if [ -f ${HOME}/bin/ttenv_swt_led ]; then
     sudo setcap "cap_sys_nice=pe" ${HOME}/bin/ttenv_swt_led
 else
     echo "${HOME}/bin/ttenv_swt_led does not exist"
